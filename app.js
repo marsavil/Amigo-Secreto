e6d0ffa36c049087b4468f8b2c5029d9dd7f8af1
marsavil <msvillaescusa@gmail.com>
2025-01-18 09:06:48 -0300

    actalizacion de archivo js

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ function agregarAmigo() {
   const input = document.querySelector('input');
   const listaHtml = document.getElementById('listaAmigos');
   const amigo = input.value.trim();
-
+  // Se verifica que el usuario haya ingresado algun nombre
   if (!amigo) {
     alert('Por favor, escribe un nombre válido.');
     return;
   }
-
+  // Se verifica que el nombre ingresado no este presente en la lista
   if (listaAmigos.includes(amigo)) {
     alert(`${amigo} ya está incluido en la lista. Prueba con un nombre distinto.`);
   } else {
+    // Se agrega el nombre ingresado a la lista
     listaAmigos.push(amigo);
     input.value = '';
     actualizarListaHtml(listaHtml);
@@ -32,14 +33,23 @@ function actualizarListaHtml(listaHtml) {
 
 function sortearAmigo() {
   const amigoSorteado  = document.getElementById('resultado')
+  // Se verifica que hayan amigos cargados
   if (listaAmigos.length === 0) {
     alert('No hay amigos en la lista. Agregue al menos un amigo.');
     return;
   }
+  // se sortea un amigo de la lista y se presenta el resultado
   const randomIndex = Math.floor(Math.random() * listaAmigos.length);
   const li = document.createElement('li');
   li.textContent = `Tu amigo secreto es ${listaAmigos[randomIndex]}`;
   amigoSorteado.appendChild(li);
+
+  // Reinicia la lista de amigos
   listaAmigos  = []
   document.getElementById('listaAmigos').innerHTML = listaAmigos
+  
+  // limpia los resultados y desja la app lista para voler a iniciar
+  setTimeout(() => {
+    amigoSorteado.innerHTML = '';
+  }, 5000);
 }
